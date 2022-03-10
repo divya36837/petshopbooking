3650d354fcdae00c38b61c5d4b7fee09bd320bf5
divya36837 <dsri36837@gmail.com>
2022-03-10 15:32:44 +0530

    added petshop files

---
+++ src/main/java/petshopbooking/petshop.java
@@ -0,0 +1,13 @@
+package petshopbooking;
+
+public class petshop {
+	public int addition()
+	{
+		int a=10;
+		int b=20;
+		
+	int c=a+b;
+	System.out.println(c);
+	return c;
+	}
+}
---
+++ src/main/java/petshopbooking/petshoptest.java
@@ -0,0 +1,10 @@
+package petshopbooking;
+
+public class petshoptest {
+	public static void main(String args[]) {
+		petshop obj=new petshop();
+		obj.addition();
+		}
+
+}
+
